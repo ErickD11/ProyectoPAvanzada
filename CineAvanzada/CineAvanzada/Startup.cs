9383c32425e2b00070078a05042fc14b9c1a22be
ACUNA SALAS <erick.acuna2@ulatina.net>
2017-11-05 09:43:17 -0600

    CineAvanzada

---
+++ CineAvanzada/CineAvanzada/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CineAvanzada.Startup))]
+namespace CineAvanzada
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
