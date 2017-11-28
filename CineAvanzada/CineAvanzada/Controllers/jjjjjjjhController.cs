19452531ab59d4d1b9521e3949c838ab51854c38
josuemora2010 <josuemora2010@gmail.com>
2017-11-27 18:16:00 -0600

    int

---
+++ CineAvanzada/CineAvanzada/Controllers/jjjjjjjhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CineAvanzada.Controllers
+{
+    public class jjjjjjjhController : Controller
+    {
+        // GET: jjjjjjjh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
